156e1b55414f4d8ad168e8d6a517d090bf4fb123
Hamad Zia <hamadzia74@gmail.com>
2025-04-22 15:47:56 +0500

    Adding a "Favorite Places" List

---
+++ components/Places/PlacesList.js
@@ -0,0 +1,16 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const PlacesList = ({ places }) => {
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={() => {}}
+    />
+  );
+};
+
+export default PlacesList;
+
+const styles = StyleSheet.create({});
