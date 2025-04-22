0dc119b37da2c4f9281131f2ecb0378d6a922e76
Hamad Zia <hamadzia74@gmail.com>
2025-04-22 17:56:35 +0500

    Getting Started with the Location Picker

---
+++ components/Places/LocationPicker.js
@@ -0,0 +1,37 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import { Colors } from "../../constants/colors";
+
+const LocationPicker = () => {
+  function getLocationHandler() {}
+  function pickOnMapHandler() {}
+
+  return (
+    <View>
+      <View style={styles.mapPreview}></View>
+      <View style={styles.actions}>
+        <OutlinedButton icon="location">Locate User</OutlinedButton>
+        <OutlinedButton icon="map">Pcik on Map</OutlinedButton>
+      </View>
+    </View>
+  );
+};
+
+export default LocationPicker;
+
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+});
