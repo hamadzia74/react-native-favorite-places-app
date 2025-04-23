e393711a0513008a7514191516b20bb556dfe020
Hamad Zia <hamadzia74@gmail.com>
2025-04-23 16:53:09 +0500

    Adding the Place Details Screen

--- screens/PlaceDetails.js
+++ screens/PlaceDetails.js
@@ -1,13 +1,48 @@
+import { Image, ScrollView } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
+import OutlinedButton from "../components/UI/OutlinedButton";
+import { Colors } from "../constants/colors";
 
-const PlaceDetails = () => {
+const PlaceDetails = ({ route }) => {
+  function showOnMapHandler() {}
+
+  const selectedPlaceId = route.params.placeId;
+  useEffect(() => {}, [selectedPlaceId]);
+
   return (
-    <View>
-      <Text>PlaceDetails</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} />
+      <View style={styles.locationContainer}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.address}></Text>
+        </View>
+        <OutlinedButton icon="map" onPress={showOnMapHandler}>
+          View on Map
+        </OutlinedButton>
+      </View>
+    </ScrollView>
   );
 };
 
 export default PlaceDetails;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: "35%",
+    minHeight: 300,
+    width: "100%",
+  },
+  locationContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addressContainer: {
+    padding: 20,
+  },
+  address: {
+    color: Colors.primary500,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
